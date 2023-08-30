41234770d9478987192f5d1434ed938748ac4f0b
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2023-08-31 00:33:22 +0800

    Update 02_prepare_package_FW3.sh

--- SCRIPTS/02_prepare_package_FW3.sh
+++ SCRIPTS/02_prepare_package_FW3.sh
@@ -5,8 +5,6 @@ sed -i 's/Os/O2/g' include/target.mk
 #sed -i 's/O2/O3/g' ./rules.mk
 # Update feed
 ./scripts/feeds update -a && ./scripts/feeds install -a
-# Irqbalance
-sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
 # rm SNAPSHOT tag
 sed -i 's,-SNAPSHOT,,g' include/version.mk
 sed -i 's,-SNAPSHOT,,g' package/base-files/image-config.in
