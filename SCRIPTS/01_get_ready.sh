0ad8588ab13490f1c92535d3aef1b410448ee29a
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2023-10-11 02:14:08 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -42,8 +42,9 @@ wait
 find openwrt/package/* -maxdepth 0 ! -name 'firmware' ! -name 'kernel' ! -name 'base-files' ! -name 'Makefile' -exec rm -rf {} +
 rm -rf ./openwrt_snap/package/base-files/image-config.in ./openwrt_snap/package/Makefile
 cp -rf ./openwrt_snap/package/* ./openwrt/package/
-cp -rf ./openwrt_snap/target/linux/* ./openwrt/target/linux/*
+cp -rf ./openwrt_snap/target/linux/* ./openwrt/target/linux/
 cp -rf ./openwrt_snap/include/* ./openwrt/include/
+cp -rf ./openwrt_snap/toolchain/* ./openwrt/toolchain/
 cp -rf ./openwrt_snap/feeds.conf.default ./openwrt/feeds.conf.default
 
 exit 0
