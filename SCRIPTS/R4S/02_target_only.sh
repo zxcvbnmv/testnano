aa9ea67b85203b14edd02c1d017c38ffe26f1367
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2023-10-12 16:31:48 +0800

    Update 02_target_only.sh

--- SCRIPTS/R4S/02_target_only.sh
+++ SCRIPTS/R4S/02_target_only.sh
@@ -7,6 +7,7 @@ sed -i 's,-mcpu=generic,-march=armv8-a+crc+crypto,g' include/target.mk
 
 #Vermagic
 latest_version="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][3-9]/p' | sed -n 1p | sed 's/v//g' | sed 's/.tar.gz//g')"
+wget https://downloads.openwrt.org/releases/${latest_version}/targets/rockchip/armv8/packages/Packages.gz
 zgrep -m 1 "Depends: kernel (=.*)$" Packages.gz | sed -e 's/.*-\(.*\))/\1/' >.vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
