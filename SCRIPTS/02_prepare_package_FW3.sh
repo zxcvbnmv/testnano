8d6b62b69e550c8bd10bf0080e014bd4822e2ac2
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2023-10-27 02:58:20 +0800

    Update 02_prepare_package_FW3.sh

--- SCRIPTS/02_prepare_package_FW3.sh
+++ SCRIPTS/02_prepare_package_FW3.sh
@@ -22,8 +22,8 @@ rm -rf package/libs/openssl
 git clone -b dev https://github.com/sbwml/package_libs_openssl package/libs/openssl
 rm -rf feeds/packages/net/curl
 cp -rf ../openwrt_pkg_ma/packages/net/curl feeds/packages/net/curl
-cp -rf ../openwrt_pkg_ma/package/libs/nghttp3 package/libs/nghttp3
-cp -rf ../openwrt_pkg_ma/package/libs/ngtcp2 package/libs/ngtcp2
+cp -rf ../openwrt_pkg_ma/packages/libs/nghttp3 package/libs/nghttp3
+cp -rf ../openwrt_pkg_ma/packages/libs/ngtcp2 package/libs/ngtcp2
 # fstool
 wget -qO - https://github.com/coolsnowwolf/lede/commit/8a4db76.patch | patch -p1
 # patch BBRv3
