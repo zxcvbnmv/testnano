3934bfebb1ab436d3c0243ce2efb3ca2cf9b68b5
zxcvbnmv <40834572+zxcvbnmv@users.noreply.github.com>
2023-11-12 06:39:04 +0800

    Update 02_prepare_package_FW3.sh

--- SCRIPTS/02_prepare_package_FW3.sh
+++ SCRIPTS/02_prepare_package_FW3.sh
@@ -114,9 +114,8 @@ cp -rf ../immortalwrt_pkg/utils/coremark ./feeds/packages/utils/coremark
 sed -i "s,-O3,-Ofast -funroll-loops -fpeel-loops -fgcse-sm -fgcse-las,g" feeds/packages/utils/coremark/Makefile
 cp -rf ../immortalwrt_23/package/utils/mhz ./package/utils/mhz
 # Add R8168 driver
-git clone -b master --depth 1 https://github.com/BROBIRD/openwrt-r8168.git package/new/r8168
-cp -f ../PATCH/r8168/Makefile ./package/new/r8168/src
-patch -p1 <../PATCH/r8168/r8168-fix_LAN_led-for_r4s-from_TL.patch
+cp -rf ../immortalwrt/package/kernel/r8168 package/new/r8168
+cp -f ../PATCH/r8168/1001-r8168-disable-eee.patch ./package/new/r8168/patches
 # igc-fix
 cp -rf ../lede/target/linux/x86/patches-5.15/996-intel-igc-i225-i226-disable-eee.patch ./target/linux/x86/patches-5.15/996-intel-igc-i225-i226-disable-eee.patch
 # Golang
